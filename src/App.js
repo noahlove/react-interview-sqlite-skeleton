b3f8af23069434e4fb6517d240817a3dadafdf75
Noah Love <noah.love@columbia.edu>
2023-09-19 10:45:06 -0400

    Showed tasks + example data pull

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+import TaskList from './TaskList';
 
 function App() {
+  const [totalInventoryValue, setTotalInventoryValue] = useState(null);
+
+  useEffect(() => {
+    // Fetch products from server
+    fetch('http://localhost:3005/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setTotalInventoryValue(data.totalInventoryValue);
+      });
+  }, []);
+
+  if (totalInventoryValue === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Total Inventory Value: ${totalInventoryValue.toFixed(2)}</div>
+        <div className="App">
+          <TaskList />
+        </div>
       </header>
     </div>
   );
